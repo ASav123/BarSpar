49797537f8e5be3cf0b84c1b4b32c95ed57ed5e0
ASav123 <144723769+ASav123@users.noreply.github.com>
2025-01-08 21:14:56 -0500

    Added Player Movement With Camera Following Player,  Created Base Class for all  Characters and added Difficulty Options on Main Menu

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    //Common attributes for all game characters
+    private string _name;
+    private int _maxHealth;
+    private int _health;
+    private int _damage;
+    
+    //Base constrctor 
+    public void CharacterCreate(string name, int maxHealth, int damage) {
+        this._name = name;
+        this._health = maxHealth;
+        this._maxHealth = maxHealth;
+        this._damage = damage; 
+    }
+
+    //Getters and setters
+
+    // Name
+    public string GetName() {
+        return this._name;
+    }
+
+    // Health
+    public int GetHealth() {
+        return this._health;
+    }
+
+    public void ChangeHealth(int changeBy) {
+        if ((changeBy + this._health) > this._maxHealth)
+        {
+            this._health = this._maxHealth;
+        }
+        else if ((changeBy + this._health) < 0) {
+            this._health = 0;
+        }
+    }
+
+    // Damage
+    public int GetDamage() {
+        return this._damage;
+    }
+
+    public void ChangeDamage(int changeBy) {
+        if ((changeBy + this._damage) < 0) {
+            this._damage = 0;
+        }
+    }
+
+
+}
