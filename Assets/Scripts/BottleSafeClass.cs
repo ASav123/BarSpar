887fb3aab63c6c66d530f6b337657a35495b9d08
Domino-wolf <125303827+Domino-wolf@users.noreply.github.com>
2025-01-09 11:51:27 -0500

    edited the safe class so that inheritance works better (made it public), and added a wine, beer, and elixir class and inherited from bottle

--- Assets/Scripts/BottleSafeClass.cs
+++ Assets/Scripts/BottleSafeClass.cs
@@ -2,16 +2,16 @@
 // Base class that tells us if a bottle is safe to drink (a property of the bottle). Can be inherited by most bottles (the safe ones)
 class Safe
 {
-    private bool _isSafe;
+    public bool IsSafe;
 
     public Safe(bool isSafe = true)
     {
-        _isSafe = isSafe;
+        this.IsSafe = isSafe;
     }
 
     public bool GetIsSafe()
     {
-        return _isSafe;
+        return this.IsSafe;
     }
 }
 
