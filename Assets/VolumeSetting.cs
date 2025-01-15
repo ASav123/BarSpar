c536684eb79206028186fca7706385f757eda5cb
prodbyrali <troybere@gmail.com>
2025-01-15 12:39:28 -0500

    Audio setting added

---
+++ Assets/VolumeSetting.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class VolumeSetting : MonoBehaviour
+{
+    [SerializeField] private AudioMixer MyMixer;
+    [SerializeField] private Slider musicSlider;
+    
+        
+    
+}
