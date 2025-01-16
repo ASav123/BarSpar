7bd25f5a8d2aac0305989fa8282669bed9bd8ac7
ASav123 <144723769+ASav123@users.noreply.github.com>
2025-01-15 21:13:51 -0500

    Added Searching and Sorting Algorithems for the Pockets Class that Organizes what the Player Picks Up

--- Assets/Scripts/Pocket.cs
+++ Assets/Scripts/Pocket.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -35,28 +36,106 @@ public class Pocket : MonoBehaviour
         Debug.Log("");
     }
 
-    //public void Sort() {
-    //    for(int i = 0; i < this._playerPocket.Length)
-    //}
+    public void Main(string[] args)
+    {
+        string[] sortingOrder = new string[] { "coin", "flower", "heart", "key" };
+        string[] pockets = new string[] { "coin", "key", "coin", "heart", "flower", "heart", "heart", "heart", "coin" };
+        InstanceSort(pockets);
+        foreach (string item in pockets)
+        {
+            Console.WriteLine(item);
+        }
+    }
 
-    //    public void Sort() {
-    //        for (int i = 0; i < this._playerPocket.Length; i++) {
-    //            if (this._playerPocket[i] == "Coin") {
-    //                this._countCoins++;
-    //            }
-    //            else if (this._playerPocket[i] == "Heart")
-    //            {
-    //                this._countHearts++;
-    //            }
-    //            else if (this._playerPocket[i] == "Key")
-    //            {
-    //                this._countKeys++;
-    //            }
-    //            else if (this._playerPocket[i] == "Flower")
-    //            {
-    //                this._countFlowers++;
-    //            }
-    //        }
-    //    }
+    public void Sort(string[] arr, string[] sortingOrder)
+    {
+        foreach (string item in sortingOrder)
+        {
+            for (int cursor = 0; cursor < arr.Length; cursor++)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[cursor] != item && arr[i] == item)
+                    {
+                        string tempItem = arr[i];
+                        arr[i] = arr[cursor];
+                        arr[cursor] = tempItem;
+                    }
+
+                }
+            }
+        }
+    }
+
+    public void InstanceSort(string[] arr)
+    {
+        int coinCount = 0;
+        int heartCount = 0;
+        int flowerCount = 0;
+        int keyCount = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == "coin")
+            {
+                coinCount++;
+            }
+            else if (arr[i] == "heart")
+            {
+                heartCount++;
+            }
+            else if (arr[i] == "flower")
+            {
+                flowerCount++;
+            }
+            else
+            {
+                keyCount++;
+            }
+        }
+        string[] tempArr = new string[4];
+        if (coinCount > heartCount)
+        {
+            tempArr[0] = "coin";
+            tempArr[1] = "heart";
+        }
+        else
+        {
+            tempArr[0] = "heart";
+            tempArr[1] = "coin";
+        }
+        tempArr[2] = "flower";
+        tempArr[3] = "key";
+
+        Sort(arr, tempArr);
+    }
+
+    // Make a method that uses this to remove the index from pocket aray
+    public int BinarySearch(string[] arr, string target)
+    {
+        int left = 0;
+        int right = arr.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            int comparison = string.Compare(arr[mid], target, System.StringComparison.Ordinal);
+
+            if (comparison == 0)
+            {
+                return mid;
+            }
+            else if (comparison < 0)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
 
 }
